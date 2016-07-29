5d3c396c2bf3784f6c03299c1c948a701cd7bb47
Chih-Wei Fang <dodo0822@hs.ntnu.edu.tw>
2016-07-29 14:14:25 +0800

    connect with backend

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ require('bootstrap');
 var tplHome = require('./templates/links.html');
 var tplContact = require('./templates/contact.html');
 
-//var dataCourses = require('./data/courses.json');
+var apiPath = 'http://54.64.171.58/api';
 
 var Rlite = require('rlite-router');
 var r = new Rlite();
@@ -18,16 +18,50 @@ r.add('', function() {
 		$(this).removeClass('active');
 	});
 
-	$('#page').html(tplHome);
-	$('#well-left-1').css('height', $('#well-right-1').height() - 15);
-	$('#well-left-2').css('height', $('#well-right-2').height() - 15);
-	//$('#well-left-3').css('height', $('#well-right-3').height() - 15);
+	$.ajax(apiPath + '/course', {
+		success: function(resp) {
+			resp = resp.results;
+			for(var i = 0; i < resp.length; ++i) {
+				var left = $('<div id="well-left-' + (i+1) + '" class="vertical-align"></div>');
+				left.text(resp[i].name);
+				$('#well-left').append(left);
+				var right = $('<div id="well-right-' + (i+1) + '"></div>');
+				for(var j = 0; j < resp[i].subcategories.length; ++j) {
+					var subcat = resp[i].subcategories[j];
+					var row = $('<div class="row"></div>');
+					var title = $('<div class="col-xs-3"></div>');
+					title.text(subcat.name);
+					var col1 = $('<div class="col-xs-5"></div>');
+					var col2 = $('<div class="col-xs-4"></div>');
+					for(var k = 0; k < subcat.course.length; k += 2) {
+						var link = $('<a href="#"></a>');
+						link.text(subcat.course[k]);
+						if(k > 0) col1.append('<br>');
+						col1.append(link);
+					}
+					for(var k = 1; k < subcat.course.length; k += 2) {
+						var link = $('<a href="#"></a>');
+						link.text(subcat.course[k]);
+						if(k > 1) col2.append('<br>');
+						col2.append(link);
+					}
+					row.append(title).append(col1).append(col2);
+					right.append(row);
+				}
+				$('#well-right').append(right);
+			}
 
-	$(window).resize(function(){
-		$('#well-left-1').css('height', $('#well-right-1').height() - 15);
-		$('#well-left-2').css('height', $('#well-right-2').height() - 15);
-		//$('#well-left-3').css('height', $('#well-right-3').height() - 15);
+			$('#well-left-1').css('height', $('#well-right-1').height() - 15);
+			$('#well-left-2').css('height', $('#well-right-2').height() - 15);
+
+			$(window).resize(function(){
+				$('#well-left-1').css('height', $('#well-right-1').height() - 15);
+				$('#well-left-2').css('height', $('#well-right-2').height() - 15);
+			});
+		}
 	});
+
+	$('#page').html(tplHome);
 });
 
 r.add('contact', function() {
